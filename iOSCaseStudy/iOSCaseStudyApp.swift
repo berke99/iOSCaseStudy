76f2a87103ec1c03696033f76628c25836c035c7
berke99 <kesginberke9@gmail.com>
2025-05-23 16:17:52 +0300

    Initial Commit

---
+++ iOSCaseStudy/iOSCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCaseStudyApp.swift
+//  iOSCaseStudy
+//
+//  Created by Berke Kesgin on 23.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
